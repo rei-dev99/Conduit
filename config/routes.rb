d2c200ae518adedb660166153041eddf527e5733
rei <idareiya123@gmail.com>
2026-03-12 07:13:37 +0900

    add: authentication_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
+  get 'signin', to: 'sessions#new'
+  post 'signin', to: 'sessions#create'
+  delete 'signin', to: 'sessions#destroy'
+
   root "articles#index"
   resources :articles
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_07_234421) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_11_214546) do
   create_table "articles", force: :cascade do |t|
     t.string "about"
     t.text "body"
@@ -31,6 +31,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_07_234421) do
   create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
   end
 
