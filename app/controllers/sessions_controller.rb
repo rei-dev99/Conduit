976ebec6f116721a772bd82054c12395780ae4d8
rei <idareiya123@gmail.com>
2026-03-14 09:05:09 +0900

    add: 認証

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,22 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to signin_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create]
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      log_in @user
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
